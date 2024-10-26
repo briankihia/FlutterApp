7431f55edba8da5af16b410458d69ed30cf8cb18
brian <brianjohnkiama@gmail.com>
2024-10-26 14:19:42 -0700

    first commit

---
+++ android/app/src/main/kotlin/brian/com/myapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package brian.com.myapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp( const MaterialApp(
+  // materialApp is going to act as a wrapper for the rest of our widgets inside
+  // the home of our material app is just going to be just a text widget.
+  // the home screen is just going to be a text widget.
+  home: Text('hey ninjas !'),
+//   different properties in our widgets are separated by commas
+));  // MaterialApp
